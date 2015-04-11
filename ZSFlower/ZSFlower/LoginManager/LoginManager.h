40a60b8e7b01e79a6d4230f6e106baeea327ed95
ouyangruoxue <zuoxiaolin@zuomatoMacBook-Pro.local>
2015-04-11 17:57:46 +0800

    增加登录的正则表达式

---
+++ ZSFlower/ZSFlower/LoginManager/LoginManager.h
@@ -0,0 +1,13 @@
+//
+//  LoginManager.h
+//  ZSFlower
+//
+//  Created by zuo xiaolin on 15/4/11.
+//  Copyright (c) 2015年 zuo xiaolin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LoginManager : NSObject
+
+@end
